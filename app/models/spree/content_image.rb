16dac0155f6189ddaaf969ec88a5c970f502dbe5
David North <davidnorth@railsdog.com>
2012-04-20 20:47:10 +0100

    Initial fixes for Spree 1.0 compatability

--- app/models/spree/content_image.rb
+++ app/models/spree/content_image.rb
@@ -1,5 +1,5 @@
-class ContentImage < Image
-  has_attached_file :attachment, 
+class Spree::ContentImage < Spree::Image
+  has_attached_file :attachment,
     :styles => {:mini => '48x48#', :large => '600x600>'},
     :default_style => :large,
     :url => "/assets/content_images/:id/:style/:basename.:extension",
